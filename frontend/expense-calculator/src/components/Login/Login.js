8a5709e83ccbf6c94c21dee79a27b9efd733017d
Kicho94 <49168212+Kicho94@users.noreply.github.com>
2020-02-26 17:56:37 +0100

    confirmed ne moze

--- frontend/expense-calculator/src/components/Login/Login.js
+++ frontend/expense-calculator/src/components/Login/Login.js
@@ -39,13 +39,10 @@ export default class Login extends React.Component {
                 ) 
             .then(res => res.json())
             .then(d=>{
-               if(!d.confirmed){
-                   alert('Please confirm your account')
-               }else {
                 localStorage.setItem('jwt', d.jwt);
                 localStorage.setItem('fullname', d.fullname);
                 window.location.reload()      
-            }         
+                    
             })
             .catch(err =>{
                 console.log(err);
